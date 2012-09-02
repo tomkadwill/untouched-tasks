23a70e48acdab63eacd589c9cf9f6efbddbc5566
Tom <tom@tom-Inspiron-1764.(none)>
2012-09-02 15:09:28 +0100

    Changed copyright header.

--- untouched_tasks/__init__.py
+++ untouched_tasks/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2009 - Luca Invernizzi <invernizzi.l@gmail.com>
+# Copyright (c) 2012 - Tom Kadwill <tomkadwill@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
