6e895788b06daaf5e40802be71a404480e43c22c
Tom Kadwill <tomkadwill@gmail.com>
2012-10-31 10:07:06 +0000

    @untouched tag now being added correctly from main menu

--- untouched_tasks/untouchedTasks.py
+++ untouched_tasks/untouchedTasks.py
@@ -23,31 +23,100 @@ from GTG import _
 from GTG.tools.logger import Log
 from GTG.tools.dates import Date
 
+###############################
+
+import sys
+import os
+
+try:
+    import pygtk
+    pygtk.require("2.0")
+except: # pylint: disable-msg=W0702
+    sys.exit(1)
+
+try:
+    import gtk
+except: # pylint: disable-msg=W0702
+    sys.exit(1)
+
+from threading import Timer
+
+from GTG.tools.logger import Log
+from GTG.tools.dates import Date
+
+##############################
+#test all of these and the ones above
+##############################
+
 
 class pluginUntouchedTasks:
-    """
-    The plugin.
-    """
 
-    def onTaskOpened(self, plugin_api):
-        """
-        Adds the button when a task is opened.
-        """
+    DEFAULT_PREFERENCES = {'max_days': 30,
+                           'is_automatic': False,
+                           'show_menu_item': True}
+
+    PLUGIN_NAME = "untouched-tasks"
+
+    #In case of automatic removing tasks, the time
+    # between two runs of the cleaner function
+    TIME_BETWEEN_PURGES = 60 * 60
+
+    def __init__(self):
+        self.path = os.path.dirname(os.path.abspath(__file__))
+        #GUI initialization
+        self.builder = gtk.Builder()
+        self.builder.add_from_file(os.path.join(
+                             os.path.dirname(os.path.abspath(__file__)) + \
+                             "/reaper.ui"))
+        self.preferences_dialog = self.builder.get_object("preferences_dialog")
+        self.pref_chbox_show_menu_item = \
+                        self.builder.get_object("pref_chbox_show_menu_item")
+        self.pref_chbox_is_automatic = \
+                        self.builder.get_object("pref_chbox_is_automatic")
+        self.pref_spinbtn_max_days = \
+                        self.builder.get_object("pref_spinbtn_max_days")
+        SIGNAL_CONNECTIONS_DIC = {
+            "on_preferences_dialog_delete_event":
+                self.on_preferences_cancel,
+            "on_btn_preferences_cancel_clicked":
+                self.on_preferences_cancel,
+            "on_btn_preferences_ok_clicked":
+                self.on_preferences_ok,
+        }
+        self.builder.connect_signals(SIGNAL_CONNECTIONS_DIC)
+        self.menu_item = gtk.MenuItem("Delete old closed tasks")
+        self.menu_item.connect('activate', self.delete_old_closed_tasks2)
+
+    def activate(self, plugin_api):
         self.plugin_api = plugin_api
-        # add a item (button) to the ToolBar
-        tb_Taskicon = gtk.Image()
-        tb_Taskicon.set_from_icon_name('mail-send', 32)
+        #preferences initialization
+        self.menu_item_is_shown = False
+        self.is_automatic = False
+        self.timer = None
+        self.preferences_load()
+        self.preferences_apply()
+        self.__log('tom reaper here?')
+        requester = self.plugin_api.get_requester()
 
-        self.tb_Taskbutton = gtk.ToolButton(tb_Taskicon)
-        self.tb_Taskbutton.set_label(_("Untouched Tasks"))
-        self.tb_Taskbutton.connect('clicked', self.onTbTaskButton, plugin_api)
-        self.tb_Taskbutton.show_all()
+    #def onTaskOpened(self, plugin_api):
+    #    """
+    #    Adds the button when a task is opened.
+    #    """
+    #    self.plugin_api = plugin_api
+    #    # add a item (button) to the ToolBar
+    #    tb_Taskicon = gtk.Image()
+    #    tb_Taskicon.set_from_icon_name('mail-send', 32)
+
+    #    self.tb_Taskbutton = gtk.ToolButton(tb_Taskicon)
+    #    self.tb_Taskbutton.set_label(_("Untouched Tasks"))
+    #    self.tb_Taskbutton.connect('clicked', self.onTbTaskButton, plugin_api)
+    #    self.tb_Taskbutton.show_all()
 	
-        plugin_api.add_toolbar_item(self.tb_Taskbutton)
+    #    plugin_api.add_toolbar_item(self.tb_Taskbutton)
 
-	#NOTE: get_textview() only works in this function
-	# gets initializes textview to use for get_insert()
-	self.textview = plugin_api.get_ui().get_textview()
+    #	#NOTE: get_textview() only works in this function
+    #	# gets initializes textview to use for get_insert()
+    #	self.textview = plugin_api.get_ui().get_textview()
 
     def deactivate(self, plugin_api):
         """
@@ -64,34 +133,14 @@ class pluginUntouchedTasks:
         Log.debug(message)
 
 ## CORE FUNCTIONS #############################################################
-    def onTbTaskButton(self, widget, plugin_api):
+    #def onTbTaskButton(self, widget, plugin_api):
+    def delete_old_closed_tasks2(self, widget):
         """
         When the user presses the button.
         """
-        task = plugin_api.get_ui().get_task()
-        Log.debug('tomkad')
-        Log.debug(task)
-        modified_time = task.get_modified()
-        Log.debug(modified_time)
-	yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-	Log.debug('yesterday')
-	Log.debug(yesterday)
+        #task = self.plugin_api.get_ui().get_task()
+        #Log.debug(task)
       
-	self.delete_old_closed_tasks() #for testing delete_old_closed_tasks
-	if modified_time > yesterday: # set to '>' to show it works but really should be '<' 
-	    itera = self.textview.get_insert()
-            if itera.starts_line():
-                self.textview.insert_text("@untouched",itera)
-            else:
-                self.textview.insert_text(" @untouched",itera)
-            self.textview.grab_focus()
-
-
-	###BEFORE YOU DO THIS###
-	#Get the current onTBTaskButton working
-	#When you click button it should get all tasks and add @untouched
-	#Then progress to adding @untouched if older than x days
-    def delete_old_closed_tasks(self, widget = None):
         self.__log("Starting deletion of old tasks")
         today = Date.today()
         max_days = 30
@@ -107,10 +156,98 @@ class pluginUntouchedTasks:
 	    Log.debug(yesterday)
       
 	    if modified_time > yesterday: # set to '>' to show it works but really should be '<' 
-	    	itera = self.textview.get_insert()
-            if itera.starts_line():
-                self.textview.insert_text("@untouched",itera)
-            else:
-                self.textview.insert_text(" @untouched",itera)
-            self.textview.grab_focus()
+		self.__log('adding @untouched to:')
+		self.__log(task)
+		task.add_tag('@untouched')
 
+	#if modified_time > yesterday: # set to '>' to show it works but really should be '<' 
+	#    itera = self.textview.get_insert()
+        #    if itera.starts_line():
+        #        self.textview.insert_text("@untouched",itera)
+        #    else:
+        #        self.textview.insert_text(" @untouched",itera)
+        #    self.textview.grab_focus()
+
+    def delete_old_closed_tasks(self, widget = None):
+        self.__log("Starting deletion of old tasks")
+        today = Date.today()
+        max_days = self.preferences["max_days"]
+        requester = self.plugin_api.get_requester()
+        closed_tree = requester.get_tasks_tree(name = 'inactive')
+        closed_tasks = [requester.get_task(tid) for tid in \
+                        closed_tree.get_all_nodes()]
+        to_remove = [t for t in closed_tasks
+                        if (today - t.get_closed_date()).days > max_days]
+
+        for task in to_remove:
+            if requester.has_task(task.get_id()):
+                requester.delete_task(task.get_id())
+
+        #If automatic purging is on, schedule another run
+        if self.is_automatic:
+            self.schedule_autopurge()
+
+
+## Preferences methods ########################################################
+    def is_configurable(self):
+        """A configurable plugin should have this method and return True"""
+        return True
+
+    def configure_dialog(self, manager_dialog):
+        self.preferences_load()
+        self.preferences_dialog.set_transient_for(manager_dialog)
+        self.pref_chbox_is_automatic.set_active(
+                        self.preferences["is_automatic"])
+        self.pref_chbox_show_menu_item.set_active(
+                        self.preferences["show_menu_item"])
+        self.pref_spinbtn_max_days.set_value(
+                        self.preferences["max_days"])
+        self.preferences_dialog.show_all()
+
+    def on_preferences_cancel(self, widget = None, data = None):
+        self.preferences_dialog.hide()
+        return True
+
+    def on_preferences_ok(self, widget = None, data = None):
+        self.preferences["is_automatic"] = \
+                self.pref_chbox_is_automatic.get_active()
+        self.preferences["show_menu_item"] = \
+                self.pref_chbox_show_menu_item.get_active()
+        self.preferences["max_days"] = \
+                self.pref_spinbtn_max_days.get_value()
+        self.preferences_apply()
+        self.preferences_store()
+        self.preferences_dialog.hide()
+
+    def preferences_load(self):
+        data = self.plugin_api.load_configuration_object(self.PLUGIN_NAME,
+                                                         "preferences")
+        if data == None or type(data) != type(dict()):
+            self.preferences = self.DEFAULT_PREFERENCES
+        else:
+            self.preferences = data
+
+    def preferences_store(self):
+        self.plugin_api.save_configuration_object(self.PLUGIN_NAME,
+                                      "preferences", self.preferences)
+
+    def preferences_apply(self):
+        #Showing the GUI
+        if self.preferences['show_menu_item'] == True and \
+                            self.menu_item_is_shown == False:
+            self.plugin_api.add_menu_item(self.menu_item)
+            self.menu_item_is_shown = True
+        elif self.preferences['show_menu_item'] == False and \
+                            self.menu_item_is_shown == True:
+            self.plugin_api.remove_menu_item(self.menu_item)
+            self.menu_item_is_shown = False
+        #Auto-purge
+        if self.preferences['is_automatic'] == True and \
+                            self.is_automatic == False:
+            self.is_automatic = True
+            # Run the first iteration immediately and schedule next iteration
+            self.delete_old_closed_tasks()
+        elif self.preferences['is_automatic'] == False and \
+                            self.is_automatic == True:
+            self.cancel_autopurge()
+            self.is_automatic = False
